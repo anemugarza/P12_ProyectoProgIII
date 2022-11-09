e738b60315ff6c3e23bfd5ec186140d0b41bd379
paulezugaza <paulezugaza@localhost>
2022-11-09 12:41:55 +0100

    ventana inicial video

--- P12_ProyectoProgIII/src/Ventanas/VentanaInicial.java
+++ P12_ProyectoProgIII/src/Ventanas/VentanaInicial.java
@@ -1,37 +1,55 @@
 package Ventanas;
 
-import java.awt.Rectangle;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+import java.awt.event.WindowListener;
 
+import uk.co.caprica.vlcj.player.base.MediaPlayer;
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 import javax.swing.JFrame;
 
-public class VentanaInicial {
-	
-	public static void main (String[] args) {
-		EmbeddedMediaPlayerComponent component = new EmbeddedMediaPlayerComponent();
-		
-		JFrame f = new JFrame();
-		f.setContentPane(component);
-		f.setBounds(new Rectangle(800,600,200,200));
-		f.addWindowListener(new WindowAdapter() {
-			
-			public void windowClosing(WindowEvent e) {
-			component.release();
-			System.exit(0);
-			}
-		});
-		f.setVisible(true);
-		
-		component.mediaPlayer().media().play("/home/mark/demo.mp4");
-	}
-	
-	
-	
-	
-	
-	
+public class VentanaInicial extends JFrame{
 
-	
+public VentanaInicial() {
+this.setSize(1000, 600);
+this.setLocationRelativeTo(null);
+this.setTitle("Fut Draft & Pack Opener 23");
+
+this.addWindowListener((WindowListener) new WindowAdapter() {
+@Override
+public void windowClosing(WindowEvent e) {
+System.exit(0);
+} 
+});
+}
+
+public static void main(String[] args) {
+VentanaInicial v = new VentanaInicial();
+EmbeddedMediaPlayerComponent comp = new EmbeddedMediaPlayerComponent() {
+public void finished(MediaPlayer mediaPlayer) {
+v.dispose();
+VentanaLogIn vl = new VentanaLogIn();
+vl.setVisible(true);
+}
+};
+v.setContentPane(comp);
+v.setVisible(true);
+comp.mediaPlayer().media().play("media/deustovideo.m4v");
+v.addKeyListener(new KeyAdapter() {
+@Override
+public void keyPressed(KeyEvent e) {
+if(e.getKeyCode() == KeyEvent.VK_SPACE ) {
+comp.mediaPlayer().controls().pause();
+v.dispose();
+VentanaLogIn vl = new VentanaLogIn();
+vl.setVisible(true);
+} 
+}
+
+});
+
+
+}
 }
