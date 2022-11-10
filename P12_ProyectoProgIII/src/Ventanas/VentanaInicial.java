5381a6ce4662cba63465f015e6e82a595396c847
anemugarza <anemugarza@localhost>
2022-11-10 15:38:16 +0100

    vlcj

--- P12_ProyectoProgIII/src/Ventanas/VentanaInicial.java
+++ P12_ProyectoProgIII/src/Ventanas/VentanaInicial.java
@@ -1,55 +1,48 @@
-package Ventanas;
 
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
 
-import uk.co.caprica.vlcj.player.base.MediaPlayer;
-import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
-import javax.swing.JFrame;
+import javax.swing.*;
+
 
 public class VentanaInicial extends JFrame{
 
-public VentanaInicial() {
-this.setSize(1000, 600);
-this.setLocationRelativeTo(null);
-this.setTitle("Fut Draft & Pack Opener 23");
-
-this.addWindowListener((WindowListener) new WindowAdapter() {
-@Override
-public void windowClosing(WindowEvent e) {
-System.exit(0);
-} 
-});
-}
-
-public static void main(String[] args) {
-VentanaInicial v = new VentanaInicial();
-EmbeddedMediaPlayerComponent comp = new EmbeddedMediaPlayerComponent() {
-public void finished(MediaPlayer mediaPlayer) {
-v.dispose();
-VentanaLogIn vl = new VentanaLogIn();
-vl.setVisible(true);
-}
-};
-v.setContentPane(comp);
-v.setVisible(true);
-comp.mediaPlayer().media().play("media/deustovideo.m4v");
-v.addKeyListener(new KeyAdapter() {
-@Override
-public void keyPressed(KeyEvent e) {
-if(e.getKeyCode() == KeyEvent.VK_SPACE ) {
-comp.mediaPlayer().controls().pause();
-v.dispose();
-VentanaLogIn vl = new VentanaLogIn();
-vl.setVisible(true);
-} 
-}
-
-});
-
-
-}
+	public VentanaInicial() {
+		this.setSize(1000, 600);
+		this.setLocationRelativeTo(null);
+		this.setTitle("Fut Draft & Pack Opener 23");
+		
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			} 
+		});
+	}
+	
+	public static void main(String[] args) {
+		VentanaInicial v = new VentanaInicial();
+		EmbeddedMediaPlayerComponent comp = new EmbeddedMediaPlayerComponent() {
+		public void finished(MediaPlayer mediaPlayer) {
+			v.dispose();
+			VentanaLogIn vl = new VentanaLogIn();
+			vl.setVisible(true);
+			}
+		};
+		v.setContentPane(comp);
+		v.setVisible(true);
+		comp.mediaPlayer().media().play("gfx/Intro.mp4");
+		v.addKeyListener(new KeyAdapter() {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if(e.getKeyCode() == KeyEvent.VK_SPACE ) {
+				comp.mediaPlayer().controls().pause();
+				v.dispose();
+				VentanaLogIn vl = new VentanaLogIn();
+				vl.setVisible(true);
+			} 
+			}
+		});
+		
+		
+	}
 }
