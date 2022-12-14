99e77783a231d1c66d6f92dae96523c209d3427c
zzbpa <zzbpa@LAPTOP-2GGU68UB>
2022-12-14 09:21:09 +0100

    Cambio de tamaño de las ventanas ajustadas al tamaño de la pantalla y nuevos metodos de Base de datos.

---
+++ P12_ProyectoProgIII/src/Ventanas/VentanaLoading.java
@@ -0,0 +1,58 @@
+package Ventanas;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+
+public class VentanaLoading extends JFrame{
+	private JPanel pTitulo,pCentro;
+	private JLabel lblTitulo;
+	private JProgressBar pb;
+	public VentanaLoading(JFrame va) {
+		super();
+		setBounds(va.getX()+10,va.getY()+10,200, 100);
+		//setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		setResizable(false);
+		pTitulo = new JPanel();
+		pCentro = new JPanel();
+		getContentPane().add(pTitulo,BorderLayout.NORTH);
+		getContentPane().add(pCentro, BorderLayout.CENTER);
+		
+		lblTitulo = new JLabel("LOADING...");
+		lblTitulo.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
+		pTitulo.add(lblTitulo);
+		JFrame v = this;
+		pb = new JProgressBar(0, 100);
+		pb.setValue(0);
+		pCentro.add(pb);
+		Runnable r = new Runnable() {
+			
+			@Override
+			public void run() {
+				for(int i=0;i<100;i++) {
+					pb.setValue(pb.getValue()+1);
+					try {
+						//Thread.sleep(100);
+						Thread.sleep(1);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+				v.dispose();
+				VentanaPrincipal ventana= new VentanaPrincipal();
+			}
+		};
+		Thread t =  new Thread(r);
+		t.start();
+		
+	}
+	
+}
