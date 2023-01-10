d519152e0e31ebda8a721cd828dca121ed795baf
zzbpa <zzbpa@LAPTOP-2GGU68UB>
2023-01-10 14:45:42 +0100

    Comienzo de la implementacion de las pruebas unitarias de la clase Logica (JUnit)

---
+++ P12_ProyectoProgIII/src/tests/LogicaTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import Clases.Usuario;
+import Logica.BaseDeDatos;
+import Logica.Logica;
+public class LogicaTest {
+
+	@Before
+	public void setUp() throws Exception {
+		BaseDeDatos.abrirConexion("MiBD.db", false);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		BaseDeDatos.cerrarConexion();
+	}
+
+	@Test
+	public void testExisteUsuario() {
+		boolean resul = Logica.existeUsuario("a@a.com");
+		assertTrue(resul);
+		resul = Logica.existeUsuario("b@b.com");
+		assertFalse(resul);
+	}
+
+	@Test
+	public void testUsuarioCorrecto() {
+		Usuario u = Logica.usuarioCorrecto("a@a.com", "a");
+		assertNotNull(u);
+		u = Logica.usuarioCorrecto("a@a.com", "b");
+		assertNull(u);
+	}
+
+}
